862bce1d603c14bf0cb114af725371534d46fc0b
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-07-11 02:48:11 -0700

    added some example cogs and the start of the BB API

---
+++ cogs/CogManager/cogs/mycog/mycog.py
@@ -0,0 +1,30 @@
+from redbot.core import commands
+from redbot.core import Config
+import os
+from git.repo.base import Repo
+
+
+class Mycog(commands.Cog):
+    """My custom cog"""
+    '''def __init__(self):
+        self.config = Config.get_conf(self, identifier=1234567890)
+        default_global = {
+            "name": "Unknown"
+        }
+        self.config.register_global(**default_global)
+    '''
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
+
+    @commands.command()
+    async def gitpull(self, ctx):
+        os.system('git pull')
+        await ctx.send("I pulled the code!")
+
+    @commands.command()
+    async def sayhi(self, ctx):
+        await ctx.send("Hello There")
