536bbba746203d1660330dc05d11b07768471c85
Nick Mark <nmgaultney@gmail.com>
2020-07-17 03:36:27 -0600

    Multiple Arguments

--- cogs/CogManager/cogs/acm_api/acm_api.py
+++ cogs/CogManager/cogs/acm_api/acm_api.py
@@ -1,9 +1,14 @@
 from redbot.core import commands
 from redbot.core import Config
 import os
+import requests
 
 
 class AcmApi(commands.Cog):
     @commands.command()
-    async def msg(self, ctx, msg):
-        await ctx.send(msg)
+    async def msg(self, ctx, msg1, msg2):
+        await ctx.send(msg1)
+        await ctx.send(msg2)
+
+    async def post(self, ctx, username, msg):
+    	await ctx.send("Message posted successfully!")
