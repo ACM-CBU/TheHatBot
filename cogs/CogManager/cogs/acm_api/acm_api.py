5be448fe95632ec7e29a9ef2e6b5c7f871c5218c
Nick Mark <nmgaultney@gmail.com>
2020-07-17 03:54:18 -0600

    API Post test

--- cogs/CogManager/cogs/acm_api/acm_api.py
+++ cogs/CogManager/cogs/acm_api/acm_api.py
@@ -5,13 +5,13 @@ import requests
 
 
 class AcmApi(commands.Cog):
-    @commands.command()
+  @commands.command()
     async def msg(self, ctx, map):
-        await ctx.send(map)
+    await ctx.send(map)
 
-    async def post(self, ctx, msg):
-        url = '10.147.19.177'
-        myobj = {"Discord": msg}
-        headers = {'Content-type': 'application/json'}
-        x = requests.post(url, data = myobj, headers=headers)
-    	await ctx.send("Message posted")
+  async def post(self, ctx, msg):
+    url = '10.147.19.177'
+    myobj = {"Discord": msg}
+    headers = {'Content-type': 'application/json'}
+    x = requests.post(url, data = myobj, headers=headers)
+    await ctx.send("Message posted")
