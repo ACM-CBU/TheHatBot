21f0dd23926ae409090366dca68c0c3de65fad8a
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-07-17 15:49:51 -0700

    added reinstallRed command and fixed updateRed command in maint cog

--- cogs/CogManager/cogs/maintenance/maintenance.py
+++ cogs/CogManager/cogs/maintenance/maintenance.py
@@ -18,5 +18,14 @@ class Maintenance(commands.Cog):
     @commands.command()
     async def updateRed(self, ctx):
         os.system('pyenv shell ACM_BOT && python -m pip install --upgrade Red-DiscordBot')
-        os.system("find /home/pi/.pyenv/versions/ACM_BOT/lib/python3.8/site-packages/redbot/ -type f -name \"*.py\" -print0 | xargs -0 sed -i '' -e 's/.is_owner/.admin_or_permissions/g'")
+        # sed -i '' -e 's/\.is_owner/.admin_or_permissions/g' s/regex/replacement/
+        os.system("find /home/pi/.pyenv/versions/ACM_BOT/lib/python3.8/site-packages/redbot/ -type f -name \"*.py\" -exec sed -r -i'' -e 's/\.is_owner/.admin_or_permissions/g' {} +")
         await ctx.send("The redbot python package has been updated and the necessary changes to the packages has been made.")
+
+    @commands.command()
+    async def reinstallRed(self, ctx):
+        os.system('pyenv shell ACM_BOT && python -m pip install --upgrade --force-reinstall Red-DiscordBot')
+        # sed -i '' -e 's/\.is_owner/.admin_or_permissions/g' s/regex/replacement/
+        os.system( "find /home/pi/.pyenv/versions/ACM_BOT/lib/python3.8/site-packages/redbot/ -type f -name \"*.py\" -exec sed -r -i'' -e 's/\.is_owner/.admin_or_permissions/g' {} +")
+        await ctx.send(
+            "The redbot python package has been updated and the necessary changes to the packages has been made.")
