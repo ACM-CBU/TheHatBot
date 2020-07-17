3c3000a40134dbdfb535085e42d5e2408f574332
Nick Mark <nmgaultney@gmail.com>
2020-07-17 03:38:01 -0600

    Multiple Arguments

--- cogs/CogManager/cogs/acm_api/acm_api.py
+++ cogs/CogManager/cogs/acm_api/acm_api.py
@@ -7,7 +7,7 @@ import requests
 class AcmApi(commands.Cog):
     @commands.command()
     async def msg(self, ctx, msg1, msg2):
-        await ctx.send(msg1)
+        await ctx.send(msg2)
         await ctx.send(msg2)
 
     async def post(self, ctx, username, msg):
