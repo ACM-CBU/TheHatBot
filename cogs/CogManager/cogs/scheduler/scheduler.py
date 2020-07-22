3c7483a4182c67410a3871a5c25ad0af1ae78614
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-07-22 11:23:14 -0700

    scheduler not getting text

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -11,6 +11,7 @@ class Scheduler(commands.Cog):
             return await ctx.send_help()
         return await ctx.send(args)
 
+    @checks.admin_or_permissions()
     @commands.command()
     async def addEvent(self, ctx: commands.Context, *args: str):
         if not args:
