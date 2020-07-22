70b51739eecdef7997cce5865a2f1aec429bf8a3
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-07-21 19:04:49 -0700

    Changed the function name of scheduler.py

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -6,7 +6,7 @@ class Scheduler(commands.Cog):
 
     @checks.admin_or_permissions()
     @commands.command()
-    async def testMessage(self, ctx: commands.Context, *args: str):
+    async def exampleCommand(self, ctx: commands.Context, *args: str):
         if not args:
             return await ctx.send_help()
         return await ctx.send(args)
