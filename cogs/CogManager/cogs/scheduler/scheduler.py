1ec28fad1ec3ac70c7568c8ed3c040f559d62d64
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-07-22 11:27:33 -0700

    the command isn't working for events

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -6,6 +6,13 @@ class Scheduler(commands.Cog):
 
     @checks.admin_or_permissions()
     @commands.command()
+    async def exampleCommand(self, ctx: commands.Context, *args: str):
+        if not args:
+            return await ctx.send_help()
+        return await ctx.send(args)
+
+    @checks.admin_or_permissions()
+    @commands.command()
     async def addEvent(self, ctx: commands.Context, *args: str):
         if not args:
             return await ctx.send_help()
