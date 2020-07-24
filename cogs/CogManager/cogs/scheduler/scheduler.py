3e89800319b610216fc7cb7e68cd5cec186ed959
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-07-23 21:47:16 -0700

    rough event hard coded

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -1,4 +1,6 @@
 from redbot.core import commands, checks
+import schedule
+import time
 from redbot.core import Config
 
 
@@ -14,6 +16,11 @@ class Scheduler(commands.Cog):
     @checks.admin_or_permissions()
     @commands.command()
     async def addEvent(self, ctx: commands.Context, *args: str):
-        if not args:
-            return await ctx.send_help()
-        return await ctx.send(args)
+        def job():
+            print("Are you bored yet")
+
+        schedule.every().thursday.at("21:49").do(job)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
