f4d9740395cc7adc49b9dc61c6bbbf8aca4646e6
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-08-03 10:14:44 -0700

    scheduler needs to be tested on discord

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -1,26 +1,55 @@
-from redbot.core import commands, checks
+from __future__ import print_function
+
 import schedule
 import time
-from redbot.core import Config
+from redbot.core import commands, checks
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 class Scheduler(commands.Cog):
 
-    @checks.admin_or_permissions()
     @commands.command()
-    async def exampleCommand(self, ctx: commands.Context, *args: str):
-        if not args:
-            return await ctx.send_help()
-        return await ctx.send(args)
+    async def events(self, ctx: commands.Context, *args: str):
+        if args.lower() == 'soon':
+            creds = None
+            # The file token.pickle stores the user's access and refresh tokens, and is
+            # created automatically when the authorization flow completes for the first
+            # time.
+            if os.path.exists('token.pickle'):
+                with open('token.pickle', 'rb') as token:
+                    creds = pickle.load(token)
+            # If there are no (valid) credentials available, let the user log in.
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json', SCOPES)
+                    creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open('token.pickle', 'wb') as token:
+                    pickle.dump(creds, token)
 
-    @checks.admin_or_permissions()
-    @commands.command()
-    async def addEvent(self, ctx: commands.Context, *args: str):
-        if not args:
-            return await ctx.send_help()
+            service = build('calendar', 'v3', credentials=creds)
 
-        schedule.every().monday.at("1:30").print(args)
+            # Call the Calendar API
+            now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+            ctx.channel.send('Getting the upcoming 10 events')
+            events_result = service.events().list(calendarId='primary', timeMin=now,
+                                                maxResults=10, singleEvents=True,
+                                                orderBy='startTime').execute()
+            events = events_result.get('items', [])
 
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+            if not events:
+                ctx.channel.send('No upcoming events found.')
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                ctx.channel.send()
