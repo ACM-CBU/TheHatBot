e5212bfaa1918cac77289165f00b310524b27e45
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-08-04 16:56:11 -0700

    credentials added on line 22 as well

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -19,7 +19,7 @@ class Scheduler(commands.Cog):
     @commands.command()
     async def events(self, ctx: commands.Context, *args: str):
         # if args.lower() == 'soon':
-        creds = None
+        creds = 'credentialcogs/CogManager/cogs/scheduler/credentials.json'
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
