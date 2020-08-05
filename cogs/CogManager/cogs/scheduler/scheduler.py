7f213fbdeadc245edfc436fc7c4378aed86113a3
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-08-04 17:16:01 -0700

    added some discord debugging statements

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -27,6 +27,7 @@ class Scheduler(commands.Cog):
             with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
+        ctx.channel.send('before creds')
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
