c58ef77ee9a6dc69d20e52af49aba3f320c28a57
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-08-11 17:37:39 -0700

    atempt to fix import statements

--- cogs/CogManager/cogs/github/github.py
+++ cogs/CogManager/cogs/github/github.py
@@ -1,7 +1,7 @@
 import os
 
 from redbot.core import checks
-from redbot.core.commands import commands
+from redbot.core.commands import commands, Context
 from .github_helper import GitHubHelper
 
 
@@ -9,18 +9,18 @@ class GitHub(commands.Cog):
 
     @checks.admin_or_permissions()
     @commands.command()
-    async def gitPull(self, ctx: commands.Context):
+    async def gitPull(self, ctx: Context):
         os.system('cd ~/TheHatBot/ && git pull')
         await ctx.send("I pulled the code!")
 
     @checks.admin_or_permissions()
     @commands.command()
-    async def gitReset(self, ctx: commands.Context):
+    async def gitReset(self, ctx: Context):
         os.system('cd ~/TheHatBot/ && git reset HEAD --hard')
         await ctx.send("I reset to the latest local git hash.")
 
     @commands.command()
-    async def addToGitHubOrganization(self, ctx, github_username):
+    async def addToGitHubOrganization(self, ctx: Context, github_username):
         if not github_username:
             await ctx.send_help()
         driver = GitHubHelper.make_driver()
