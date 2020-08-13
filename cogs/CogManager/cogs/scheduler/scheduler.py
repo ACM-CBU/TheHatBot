8a71ab32114a4440adcca0833a6a3f421f1ec0fc
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-08-12 18:37:53 -0700

    class named incorrectly

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -14,75 +14,74 @@ SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 
 client = discord.Client()
 
+class Scheduler(commands.Cog):
+    async def events(self, ctx: commands.Context, *args: str):
+        print("Upcoming")
+        now = datetime.datetime.now()
+        run_at = now + timedelta(hours=3)
+        delay = (run_at - now).total_seconds()
+        #Begin Google Calander API
+        store = oauth2client.file.Storage('cogs/CogManager/cogs/scheduler/creds.json')
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = oauth2client.client.flow_from_clientsecrets('credentials.json', SCOPES)
+            creds = oauth2client.tools.run_flow(flow, store)
+        service = build('calendar', 'v3', http=creds.authorize(Http()))
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+            #Retrieve all Events
+        events_result = service.events().list(calendarId='CENSORED', timeMin=now,maxResults=250, singleEvents=True,orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+            #Currentmode stores if searching for tommorow, the week, or other (1,2,3)
+        Currentmode = 0
+        UMode=1
+        none = True
+            #Gets end of week
+        dt = datetime.datetime.now()
+        startweek = dt - timedelta(days=dt.weekday())
+        endweek = startweek + timedelta(days=6)
+        dtstring = str(dt.date())
+        TheMessages = "**" + dtstring + " Report**"
+        ctx.channel.send(TheMessages)
+        if not events:
+            ctx.channel.send('No upcoming events')
+        for event in events:
+
+            if Currentmode == 0:
+                ctx.channel.send('Tommorow')
+                Currentmode = 1
+
+            thestr = event['start'].get('dateTime')
+
+            count = 0
+            count2 = 0
+            for x in thestr:
+                count += 1
+                if x == "-":
+                    count2 +=1
+                    if count2 == 3:
+                        break
+            count = count - 1
+            thestr = thestr[0:count]
 
 
-async def events(self, ctx: commands.Context, *args: str):
-    print("Upcoming")
-    now = datetime.datetime.now()
-    run_at = now + timedelta(hours=3)
-    delay = (run_at - now).total_seconds()
-    #Begin Google Calander API
-    store = oauth2client.file.Storage('cogs/CogManager/cogs/scheduler/creds.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = oauth2client.client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = oauth2client.tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(Http()))
-    now = datetime.datetime.utcnow().isoformat() + 'Z'
-        #Retrieve all Events
-    events_result = service.events().list(calendarId='CENSORED', timeMin=now,maxResults=250, singleEvents=True,orderBy='startTime').execute()
-    events = events_result.get('items', [])
+            start = datetime.datetime.strptime(thestr, "%Y-%m-%dT%H:%M:%S")
 
-        #Currentmode stores if searching for tommorow, the week, or other (1,2,3)
-    Currentmode = 0
-    UMode=1
-    none = True
-        #Gets end of week
-    dt = datetime.datetime.now()
-    startweek = dt - timedelta(days=dt.weekday())
-    endweek = startweek + timedelta(days=6)
-    dtstring = str(dt.date())
-    TheMessages = "**" + dtstring + " Report**"
-    ctx.channel.send(TheMessages)
-    if not events:
-        ctx.channel.send('No upcoming events')
-    for event in events:
-
-        if Currentmode == 0:
-            ctx.channel.send('Tommorow')
-            Currentmode = 1
-
-        thestr = event['start'].get('dateTime')
-
-        count = 0
-        count2 = 0
-        for x in thestr:
-            count += 1
-            if x == "-":
-                count2 +=1
-                if count2 == 3:
-                    break
-        count = count - 1
-        thestr = thestr[0:count]
-
-
-        start = datetime.datetime.strptime(thestr, "%Y-%m-%dT%H:%M:%S")
-
-        if (start - dt).days <= 7 and Currentmode == 1:
-            if UMode == 1:
-                ctx.channel.send('None')
-                ctx.channel.send('in the next 7 days')
-            UMode = 2
-            Currentmode = 2
-        elif (start - dt).days >= 7 and (Currentmode == 1 or Currentmode == 2):
-            if UMode == 1:
-                ctx.channel.send('None')
-                ctx.channel.send('in the next 7 days')
-                ctx.channel.send('None')
-            ctx.channel.send('Longterm')
-            Currentmode = 3
-            UMode = 3
-        FirstMessage = str(start.date())
-        SecondMessage = event['summary']
-        ThirdMessage= FirstMessage + " " + SecondMessage
-        ctx.channel.send(ThirdMessage)
+            if (start - dt).days <= 7 and Currentmode == 1:
+                if UMode == 1:
+                    ctx.channel.send('None')
+                    ctx.channel.send('in the next 7 days')
+                UMode = 2
+                Currentmode = 2
+            elif (start - dt).days >= 7 and (Currentmode == 1 or Currentmode == 2):
+                if UMode == 1:
+                    ctx.channel.send('None')
+                    ctx.channel.send('in the next 7 days')
+                    ctx.channel.send('None')
+                ctx.channel.send('Longterm')
+                Currentmode = 3
+                UMode = 3
+            FirstMessage = str(start.date())
+            SecondMessage = event['summary']
+            ThirdMessage= FirstMessage + " " + SecondMessage
+            ctx.channel.send(ThirdMessage)
