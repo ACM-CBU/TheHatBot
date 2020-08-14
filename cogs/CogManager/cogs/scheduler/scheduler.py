a33fcc3193f4cb57a9146c526be3274978277f59
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-08-13 21:58:56 -0700

    schedule ready for discord testing

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -1,16 +1,14 @@
 from __future__ import print_function
 
-import datetime
 import os
 import pickle
-from datetime import timedelta
+import datetime
 
 import discord
+from google_auth_httplib2 import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from httplib2 import Http
-import oauth2client
-from redbot.core import commands, checks
+from redbot.core import commands
 
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 
@@ -19,80 +17,46 @@ client = discord.Client()
 
 class Scheduler(commands.Cog):
 
-    @checks.admin_or_permissions()
-    @commands.command()
-    async def events(self, ctx: commands.Context, *args: str):
-        print("Upcoming")
-        now = datetime.datetime.now()
-        run_at = now + timedelta(hours=3)
-        delay = (run_at - now).total_seconds()
-
-        # Begin Google Calander API
+    @staticmethod
+    def build_google_calendar_service():
+        SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+        """Shows basic usage of the Google Calendar API.
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
         creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
         if os.path.exists('token.pickle'):
             with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
-            # If there are no (valid) credentials available, let the user log in.
+        # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
-            flow = InstalledAppFlow.from_client_secrets_file('cogs/CogManager/cogs/scheduler/creds.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        service = build('calendar', 'v3', http=creds.authorize(Http()))
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-        # Get all Events
-        events_result = service.events().list(calendarId='CENSORED', timeMin=now, maxResults=250, singleEvents=True,
+        return build('calendar', 'v3', credentials=creds)
+
+    @commands.command()
+    async def getEvents(self, ctx: commands.Context):
+        # Call the Calendar API
+        service = self.build_google_calendar_service()
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        await ctx.send('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
                                               orderBy='startTime').execute()
         events = events_result.get('items', [])
 
-        # Currentmode stores if searching for tommorow, the week, or other (1,2,3)
-        Currentmode = 0
-        UMode = 1
-        none = True
-        # Gets end of week
-        dt = datetime.datetime.now()
-        startweek = dt - timedelta(days=dt.weekday())
-        endweek = startweek + timedelta(days=6)
-        dtstring = str(dt.date())
-        TheMessages = "**" + dtstring + " Report**"
-        ctx.send(TheMessages)
         if not events:
-            ctx.send('No upcoming events')
+            await ctx.send('No upcoming events found.')
         for event in events:
-
-            if Currentmode == 0:
-                ctx.send('Tommorow')
-                Currentmode = 1
-
-            thestr = event['start'].get('dateTime')
-
-            count = 0
-            count2 = 0
-            for x in thestr:
-                count += 1
-                if x == "-":
-                    count2 += 1
-                    if count2 == 3:
-                        break
-            count = count - 1
-            thestr = thestr[0:count]
-
-            start = datetime.datetime.strptime(thestr, "%Y-%m-%dT%H:%M:%S")
-
-            if (start - dt).days <= 7 and Currentmode == 1:
-                if UMode == 1:
-                    ctx.send('None')
-                    ctx.send('in the next 7 days')
-                UMode = 2
-                Currentmode = 2
-            elif (start - dt).days >= 7 and (Currentmode == 1 or Currentmode == 2):
-                if UMode == 1:
-                    ctx.send('None')
-                    ctx.send('in the next 7 days')
-                    ctx.send('None')
-                ctx.send('Longterm')
-                Currentmode = 3
-                UMode = 3
-            FirstMessage = str(start.date())
-            SecondMessage = event['summary']
-            ThirdMessage = FirstMessage + " " + SecondMessage
-            ctx.send(ThirdMessage)
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            await ctx.send(str((start, event['summary'])))
