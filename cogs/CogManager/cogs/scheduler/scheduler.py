1b10ca81a1f8af5ceef1b17afa438f00d4896a75
Lauren Nelson <laurennelson@Laurens-MacBook-Pro.local>
2020-08-13 21:08:01 -0700

    ctx.send for dm purposes

--- cogs/CogManager/cogs/scheduler/scheduler.py
+++ cogs/CogManager/cogs/scheduler/scheduler.py
@@ -14,8 +14,8 @@ from redbot.core import commands, checks
 
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 
-
 client = discord.Client()
+
 
 class Scheduler(commands.Cog):
 
@@ -27,9 +27,7 @@ class Scheduler(commands.Cog):
         run_at = now + timedelta(hours=3)
         delay = (run_at - now).total_seconds()
 
-        #Begin Google Calander API
-        # store = oauth2client.file.Storage('cogs/CogManager/cogs/scheduler/creds.json')
-        # creds = store.get()
+        # Begin Google Calander API
         creds = 'cogs/CogManager/cogs/scheduler/creds.json'
         if os.path.exists('token.pickle'):
             with open('token.pickle', 'rb') as token:
@@ -41,27 +39,28 @@ class Scheduler(commands.Cog):
         service = build('calendar', 'v3', http=creds.authorize(Http()))
         now = datetime.datetime.utcnow().isoformat() + 'Z'
 
-        #Get all Events
-        events_result = service.events().list(calendarId='CENSORED', timeMin=now,maxResults=250, singleEvents=True,orderBy='startTime').execute()
+        # Get all Events
+        events_result = service.events().list(calendarId='CENSORED', timeMin=now, maxResults=250, singleEvents=True,
+                                              orderBy='startTime').execute()
         events = events_result.get('items', [])
 
-        #Currentmode stores if searching for tommorow, the week, or other (1,2,3)
+        # Currentmode stores if searching for tommorow, the week, or other (1,2,3)
         Currentmode = 0
-        UMode=1
+        UMode = 1
         none = True
-            #Gets end of week
+        # Gets end of week
         dt = datetime.datetime.now()
         startweek = dt - timedelta(days=dt.weekday())
         endweek = startweek + timedelta(days=6)
         dtstring = str(dt.date())
         TheMessages = "**" + dtstring + " Report**"
-        ctx.channel.send(TheMessages)
+        ctx.send(TheMessages)
         if not events:
-            ctx.channel.send('No upcoming events')
+            ctx.send('No upcoming events')
         for event in events:
 
             if Currentmode == 0:
-                ctx.channel.send('Tommorow')
+                ctx.send('Tommorow')
                 Currentmode = 1
 
             thestr = event['start'].get('dateTime')
@@ -71,30 +70,29 @@ class Scheduler(commands.Cog):
             for x in thestr:
                 count += 1
                 if x == "-":
-                    count2 +=1
+                    count2 += 1
                     if count2 == 3:
                         break
             count = count - 1
             thestr = thestr[0:count]
 
-
             start = datetime.datetime.strptime(thestr, "%Y-%m-%dT%H:%M:%S")
 
             if (start - dt).days <= 7 and Currentmode == 1:
                 if UMode == 1:
-                    ctx.channel.send('None')
-                    ctx.channel.send('in the next 7 days')
+                    ctx.send('None')
+                    ctx.send('in the next 7 days')
                 UMode = 2
                 Currentmode = 2
             elif (start - dt).days >= 7 and (Currentmode == 1 or Currentmode == 2):
                 if UMode == 1:
-                    ctx.channel.send('None')
-                    ctx.channel.send('in the next 7 days')
-                    ctx.channel.send('None')
-                ctx.channel.send('Longterm')
+                    ctx.send('None')
+                    ctx.send('in the next 7 days')
+                    ctx.send('None')
+                ctx.send('Longterm')
                 Currentmode = 3
                 UMode = 3
             FirstMessage = str(start.date())
             SecondMessage = event['summary']
-            ThirdMessage= FirstMessage + " " + SecondMessage
-            ctx.channel.send(ThirdMessage)
+            ThirdMessage = FirstMessage + " " + SecondMessage
+            ctx.send(ThirdMessage)
