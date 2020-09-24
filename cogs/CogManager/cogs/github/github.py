14e206f1e0cfbd3b7b079b33e9cc1fb949e36d3c
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-09-24 13:49:47 -0700

    updated github cog

--- cogs/CogManager/cogs/github/github.py
+++ cogs/CogManager/cogs/github/github.py
@@ -1,23 +1,29 @@
-import os
+from pathlib import Path
+
 import requests
+from git import Repo
 from redbot.core import checks
 from redbot.core.commands import commands, Context
+
 from .github_helper import GitHubHelper
 
 
 class GitHub(commands.Cog):
+    USER = 'bot'
+    REPO_NAME = 'TestBot1'
+    REPO_PATH = Path(f'/home/{USER}/.local/share/Red-DiscordBot/data/{REPO_NAME}/')
+    TEST_REPO_DIR = REPO_PATH.joinpath('Test_Cogs')
+    PARENT_REPO = Repo(str(REPO_PATH))
 
     @checks.admin_or_permissions()
     @commands.command()
     async def gitPull(self, ctx: Context):
-        os.system('cd ~/TheHatBot/ && git pull')
-        await ctx.send("I pulled the code!")
+        await ctx.send(self.PARENT_REPO.git.pull())
 
     @checks.admin_or_permissions()
     @commands.command()
     async def gitReset(self, ctx: Context):
-        os.system('cd ~/TheHatBot/ && git reset HEAD --hard')
-        await ctx.send("I reset to the latest local git hash.")
+        await ctx.send(self.PARENT_REPO.git.reset('HEAD', '--hard'))
 
     @commands.command()
     async def addToGitHubOrganization(self, ctx: Context, github_username):
@@ -36,8 +42,12 @@ class GitHub(commands.Cog):
     async def cloneTestRepo(self, ctx: Context, github_repo_url: str):
         if not github_repo_url:
             await ctx.send_help()
-        if 'github.com/ACM-CBU' in github_repo_url:
-            os.system('mkdir TestRepos')
-            os.system(f'cd ~/TestRepos && git clone {github_repo_url}')
+        if 'github.com/ACM-CBU/' in github_repo_url and len(github_repo_url.split('/')) > 4:
+            sub_name = github_repo_url.split('/')[-1]
+            new_sub = self.PARENT_REPO.create_submodule(name=sub_name, path=str(self.TEST_REPO_DIR),
+                                                        url=github_repo_url)
+            new_sub.set_parent_commit(None)
+            self.PARENT_REPO.git.commit(message=f"Added new Submodule: {sub_name}")
+            self.PARENT_REPO.git.push()
         else:
             await ctx.send("The repository must be within the ACM GitHub Organization")
